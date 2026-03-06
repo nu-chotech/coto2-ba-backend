e0dcd9ebd9985432d3f3563f14a95e76e3696f23
tmba <ut42.nu@gmail.com>
2026-03-06 14:45:40 +0900

    hotfix: 400 cors error

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",  # ローカル開発用
         "https://coto2-ba.vercel.app",  # 本番フロントエンド（後で変更）
+        "https://coto2-ba.ut42tech.com",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
